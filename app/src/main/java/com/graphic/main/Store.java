1e7103fde4c23dcd137fe79e1ebe8e6222179e61
Mitochondrion <richard22062004@gmail.com>
2021-10-15 18:12:59 +0300

    Changes to volume in settings now change music volume in MainActivity

--- app/src/main/java/com/graphic/main/Store.java
+++ app/src/main/java/com/graphic/main/Store.java
@@ -6,20 +6,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class Store {
-    /*
-    public static void save(Activity activity, int key, int value) {
-        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putInt(activity.getString(key), value);
-        editor.apply();
-    }
-
-    public static int read(Activity activity, int key, int defaultValue) {
-        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
-        return sharedPref.getInt(activity.getString(key), defaultValue);
-    }
-    */
-
     public static void save(Context context, int key, int value) {
         SharedPreferences sharedPref = context.getSharedPreferences(context.getString(R.string.shared_pref_key), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
